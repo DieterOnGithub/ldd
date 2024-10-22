8f08e54c5ffe0511cc0c77c1d2f7c2aeca25321f
DieterOnGithub <dieter@kribbel.at>
2024-10-22 16:45:41 +0200

    Testing Debian setup

--- hello.c
+++ hello.c
@@ -3,5 +3,6 @@
 int main(void) {
     printf("Hello world from the Ubuntu virtual machine!\n");
     printf("And now some changes!\n");
+    printf("And more changes from the new system!\n");
     return 0;
 }
