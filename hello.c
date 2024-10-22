191ba81d3307aa8fff574d3b4fb16bf87c3443e3
DieterOnGithub <dieter@kribbel.at>
2024-10-22 16:53:21 +0200

    Clean slate

--- hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    printf("Hello world from the Ubuntu virtual machine!\n");
-    printf("And now some changes!\n");
-    printf("And more changes from the new system!\n");
-    return 0;
-}
